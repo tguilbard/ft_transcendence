9d53f1743d86a873e5787c1922e8e96722e3dbf4
Taylee BIGOT <tbigot@e1r13p7.42.fr>
2021-11-25 13:29:21 +0100

    clear

--- setup.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/nestjs/typescript-starter.git project
-cd project
-npm i --save @nestjs/websockets @nestjs/platform-socket.io express canvas datauri jsdom
-
-
-rm -rf src ; cp -r ../src src
-npm run start:dev
