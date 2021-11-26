108de4d5966edbaa19fd55c88d6fb151dcec6015
tguilbard <tanguy.guilbard@free.fr>
2021-11-26 11:47:25 +0100

    UPDATE chat + REPAIR chatcord

--- chat/Chat.js
+++ chat/Chat.js
@@ -1,6 +1,31 @@
 const chatForm = document.getElementById('chat-form');
-const chatText = document.getElementById('chat');
+const chatMessages = document.querySelector('.chat');
+const channel = document.getElementById('channel');
 
+let muted = [];
+let room = "test";
+let username;
+
+const socket = io();
+
+socket.on("connect", () => {
+  username = socket.id;
+});
+
+socket.emit('joinRoom', { username, room });
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  // outputUsers(users);
+});
+
+socket.on('message', (message) => {
+  console.log(message);
+  if (muted.indexOf(message.username) == -1)
+    outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -14,7 +39,18 @@ chatForm.addEventListener('submit', (e) => {
       return false;
     }
 
-    printChat(msg);
+    if (msg.substring(0, 6) == "/mute ") {
+      if (muted.indexOf(msg.substring(6)) == -1)
+        muted.push(msg.substring(6));
+    }
+    else if (msg.substring(0, 8) == "/unmute ") {
+      let index;
+      index = muted.indexOf(msg.substring(8));
+      if (index != -1)
+        muted.splice(index, 1);
+    }
+    else
+      socket.emit('chatMessage', msg);
     
     // Clear input
     e.target.elements.msg.value = '';
@@ -22,8 +58,30 @@ chatForm.addEventListener('submit', (e) => {
 
 });
 
-function printChat(toDraw)
-{
-    chatText.innerHTML += toDraw;
-    chatText.innerHTML += "<br>";
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message;
+  div.appendChild(para);
+  document.querySelector('.chat').appendChild(div);
 }
+
+function outputRoomName(room) {
+  channel.innerText = room;
+}
+
+// function outputUsers(users) {
+//   userList.innerHTML = '';
+//   users.forEach((user) => {
+//     const li = document.createElement('li');
+//     li.innerText = user.username;
+//     userList.appendChild(li);
+//   });
+// }
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-git clone https://github.com/nestjs/typescript-starter.git project
+if ! [ -d "./project" ];
+then
+    git clone https://github.com/nestjs/typescript-starter.git project
+fi
 cd project
 npm i --save @nestjs/websockets @nestjs/platform-socket.io express canvas datauri jsdom
-
 
 rm -rf src ; cp -r ../src src
 npm run start:dev
