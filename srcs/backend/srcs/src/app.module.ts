5729714d22f716eed988ff7619fbb173e6b95707
tayschee <tbigot@student.42.fr>
2021-12-09 12:43:44 +0100

    backend with env variable

--- srcs/backend/srcs/src/app.module.ts
+++ srcs/backend/srcs/src/app.module.ts
@@ -1,6 +1,6 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { TfaModule } from './tfa/tfa.module';
@@ -13,23 +13,29 @@ import { UsersService } from './users/users.service';
 // import DatabaseFilesService from './Photo/databaseFiles.service'
 import { UsersController } from './users/users.controller';
 import { AuthMiddleware } from './middleware/resgister.middleware';
+import { type } from 'os';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      ignoreEnvFile: true
+      ignoreEnvFile: true, //any .env file
+      //cache: true //More fastest
    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        inject: [ConfigService],
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: true
+      }),
   }),
     UsersModule,
     TfaModule,
